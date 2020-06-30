fc62f52b0e92c75fd2e80732d32c81ce689d8ab7
github username <github email address>
2020-06-30 13:01:19 -0700

    temporarily display SignupForm Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm.js'
 import Logout from './components/Logout.js'
+import SignupForm from './components/SignupForm.js'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 
@@ -16,6 +17,7 @@ class App extends Component {
             <div className="App">
                 <LoginForm />
                 <Logout />
+                <SignupForm />
             </div>
         )
     }
