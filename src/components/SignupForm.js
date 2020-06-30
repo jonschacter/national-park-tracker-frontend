b5ab5f5f530c56a5adf0b4022f22f29411482ea6
github username <github email address>
2020-06-30 14:20:16 -0700

    add and connect signup form component

---
+++ src/components/SignupForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signup } from '../actions/currentUser.js'
+
+class SignupForm extends Component {
+    
+    state = {
+        username: "",
+        password: ""
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.signup(this.state)
+        this.setState({
+            username: "",
+            password: ""
+        })
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label>Username:</label>
+                <input type="text" name="username" onChange={this.handleChange} value={this.state.username} />
+                <br/>
+                <label>Password:</label>
+                <input type="password" name="password" onChange={this.handleChange} value={this.state.password} />
+                <br/>
+                <input type="submit" value="Sign Up" />
+            </form>
+        )
+    }
+}
+
+export default connect(null, { signup })(SignupForm)
