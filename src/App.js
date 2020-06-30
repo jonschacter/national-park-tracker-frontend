c6d5dbb4c5bcbf2104780f4993cfe83ac158f512
github username <github email address>
2020-06-30 12:00:10 -0700

    change App to class component and add dispatch to check current user when mounted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm.js'
 import Logout from './components/Logout.js'
+import { connect } from 'react-redux'
+import { getCurrentUser } from './actions/currentUser.js'
 
-function App() {
-  return (
-    <div className="App">
-      <LoginForm />
-      <Logout />
-    </div>
-  );
+class App extends Component {
+
+    componentDidMount(){
+        this.props.getCurrentUser()
+    }
+
+    render(){
+        return(
+            <div className="App">
+                <LoginForm />
+                <Logout />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App)
