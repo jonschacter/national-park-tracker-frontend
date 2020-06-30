0872e84e5de3051c79f7b6e8219b8d61677b55f4
github username <github email address>
2020-06-29 21:56:53 -0700

    temporarily display Logout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm.js'
+import Logout from './components/Logout.js'
 
 function App() {
   return (
     <div className="App">
       <LoginForm />
+      <Logout />
     </div>
   );
 }
