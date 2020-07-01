b8b56aa856a3900764fee92a34ef2abad7fbabec
github username <github email address>
2020-07-01 12:00:46 -0700

    add link to the VisitShow page

--- src/components/visit/Visit.js
+++ src/components/visit/Visit.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Visit = ({ visit, park }) => {
     return(
         <div className="visit">
-            {park ? park.name : "destination"} - {visit.start_date}
+            <Link to={`/visits/${visit.id}`}>{park ? park.name : "Destination"} - {visit.start_date}</Link>
         </div>
     )
 }
