1189d84981857ba63f16797961fea248c3b47893
github username <github email address>
2020-07-01 16:49:26 -0700

    add delete visit onClick event

--- src/components/visit/VisitShow.js
+++ src/components/visit/VisitShow.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { deleteVisit } from '../../actions/visits.js'
 
-const VisitShow = ({ visit, park }) => {
+const VisitShow = ({ visit, park, history, deleteVisit }) => {
     return(
         <div>
             <h3>{ park ? <Link to={`/parks/${park.parkCode}`}>{park.name}</Link> : "Destination" }</h3>
@@ -10,7 +11,7 @@ const VisitShow = ({ visit, park }) => {
             <p>End Date: { visit ? visit.end_date : null }</p>
             { visit ? <Link to={`/visits/${visit.id}/edit`}>Edit This Visit</Link> : null }
             <br />
-            { visit ? <Link>Delete This Visit</Link> : null }
+            { visit ? <Link onClick={event => deleteVisit(visit.id, history)}>Delete This Visit</Link> : null }
         </div>
     )
 }
@@ -19,8 +20,8 @@ const mapStateToProps = ({ visits, parks }, props) => {
     const visit = visits.find(visit => visit.id === parseInt(props.match.params.id))
     return {
         visit,
-        park: parks.find(park => park.parkCode === visit.park_code)
+        park: visit ? parks.find(park => park.parkCode === visit.park_code) : undefined
     }
 }
 
-export default connect(mapStateToProps)(VisitShow)
+export default connect(mapStateToProps, { deleteVisit })(VisitShow)
