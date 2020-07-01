a164afd4c5940387f0803abb199376a4095a9e31
github username <github email address>
2020-07-01 11:26:33 -0700

    Visits component renders lmultiple Visit Components

--- src/components/visit/Visits.js
+++ src/components/visit/Visits.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Visit from './Visit.js'
 
-const Visits = () => {
+const Visits = ({ visits, parks }) => {
+    const renderVisits = () => {
+        return visits.map(visit => {
+            return <Visit key={visit.id} visit={visit} park={parks.find(park => park.parkCode === visit.park_code)}/>
+        })
+    }
+    
     return(
-        <div>
-            VISITS
+        <div className="visits-list">
+            <h2>MY VISITS</h2>
+            { visits.length > 0 ? renderVisits() : null }
         </div>
     )
 }
 
-export default connect()(Visits)
+const mapStateToProps = ({ visits, parks }) => {
+    return {
+        visits,
+        parks
+    }
+}
+
+export default connect(mapStateToProps)(Visits)
