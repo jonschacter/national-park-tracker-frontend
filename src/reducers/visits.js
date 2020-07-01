4015d461aa09824327253e49fd6d0300e3bbd75f
github username <github email address>
2020-07-01 14:56:24 -0700

    add ADD_VISIT switch

--- src/reducers/visits.js
+++ src/reducers/visits.js
@@ -2,6 +2,11 @@ export default (state = [], action) => {
     switch(action.type) {
         case "SET_VISITS":
             return action.visits
+        case "ADD_VISIT":
+            return [
+                ...state,
+                action.visit
+            ]
         default:
             return state
     }
