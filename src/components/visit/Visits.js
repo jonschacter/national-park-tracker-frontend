83088c2e9ff2cdedbc86a194f0bdafcf5a167552
github username <github email address>
2020-07-01 11:07:34 -0700

    create Visits component

---
+++ src/components/visit/Visits.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Visits = () => {
+    return(
+        <div>
+            VISITS
+        </div>
+    )
+}
+
+export default connect()(Visits)
