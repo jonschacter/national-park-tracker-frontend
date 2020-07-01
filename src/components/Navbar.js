5a0d677c2af13742c81556db66d02fa4480206a5
github username <github email address>
2020-06-30 20:48:11 -0700

    made Parks link always display and Log Out only if a user is logged in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = ({ loggedIn }) => {
     return(
         <div>
-            <Link to="/">Home</Link>
+            <Link to="/">Home</Link> | <Link to="/parks">Parks</Link>{ loggedIn ? <> | <Link>Log Out</Link></> : null }
         </div>
     )
 }
 
-export default Navbar
+const mapStateToProps = state => {
+    return {
+        loggedIn: !!state.currentUser
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
