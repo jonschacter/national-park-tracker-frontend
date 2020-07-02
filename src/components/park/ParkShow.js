ce7326497641f353c130df44ebfb8aedc3dc80c2
github username <github email address>
2020-07-02 16:59:45 -0700

    display park reviews in show page

--- src/components/park/ParkShow.js
+++ src/components/park/ParkShow.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getReviews } from '../../actions/parkReviews.js'
 import ParkAddress from './ParkAddress.js'
 import ParkImage from './ParkImage.js'
+import ReviewCard from '../review/ReviewCard.js'
 
-const ParkShow = ({ park }) => {
-    const renderPark = () => {
+class ParkShow extends Component {
+
+    componentDidMount(){
+        this.props.getReviews(this.props.match.params.id)
+    }
+
+    renderReviews = () => {
+        return(
+            <div className="park-reviews">
+                <h3>REVIEWS</h3>
+                { this.props.parkReviews.map(review => <><ReviewCard review={review} /><br/></>) }
+            </div>
+        )
+    }
+
+    renderPark = () => {
+        const { park, parkReviews } = this.props
         return(
             <div>
                 <h2>{park.name}</h2>
@@ -13,19 +30,24 @@ const ParkShow = ({ park }) => {
                 })}
                 <p>{park.description}</p>
                 {park.images.map((image, i) => <ParkImage key={i} image={image}/>)}
+                { parkReviews.length > 0 ? this.renderReviews() : null }
             </div>
         )
     }
 
-    return(
-        <>{ park ? renderPark() : null }</>
-    )
-}
+    render(){
+        return(
+            <>{ this.props.park ? this.renderPark() : null }</>
+        )
+    }
+}    
 
-const mapStateToProps = (state, props) => {
+
+const mapStateToProps = ({ parks, parkReviews }, props) => {
     return {
-        park: state.parks.find(park => park.id === parseInt(props.match.params.id))
+        park: parks.find(park => park.id === parseInt(props.match.params.id)),
+        parkReviews
     }
 }
 
-export default connect(mapStateToProps)(ParkShow)
+export default connect(mapStateToProps, { getReviews })(ParkShow)
