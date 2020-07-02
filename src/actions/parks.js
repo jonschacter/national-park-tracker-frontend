9739d1c7234bb6ce66acdcef9c6a5d464ffadd4b
github username <github email address>
2020-07-02 11:51:16 -0700

    move getParks fetch to backend

--- src/actions/parks.js
+++ src/actions/parks.js
@@ -1,6 +1,4 @@
-import { tempParkData } from '../tempParkData.js'
-
-// const npsApiUrl = 'https://developer.nps.gov/api/v1/parks?&limit=20&api_key=OwNvFO9PgnzaBuEJMEol0fpU5JwIUYO1WJbxGbL9'
+// const npsApiUrl = 'https://developer.nps.gov/api/v1/parks?&api_key=OwNvFO9PgnzaBuEJMEol0fpU5JwIUYO1WJbxGbL9'
 
 export const setParks = parks => {
     return {
@@ -11,12 +9,10 @@ export const setParks = parks => {
 
 export const getParks = () => {
     return dispatch => {
-        // temporary fake request
-        setTimeout(() => {dispatch(setParks(tempParkData))}, 2000)
-
-        // real request
-        // return fetch(npsApiUrl)
-        //     .then(resp => resp.json())
-        //     .then(response => dispatch(setParks(response.data)))
+        return fetch('http://localhost:3001/parks')
+            .then(resp => resp.json())
+            .then(response => {
+                dispatch(setParks(response))
+            })
     }
 }
