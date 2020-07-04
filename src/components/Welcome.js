3ab0a7478c81da3debc4a703db01184446918da8
github username <github email address>
2020-07-04 15:14:35 -0700

    code cleanup

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import LogoutButton from './user/LogoutButton.js'
 import LoginButton from './user/LoginButton.js'
 import SignupButton from './user/SignupButton.js'
 
-const Welcome = ({ currentUser, history }) => {
+const Welcome = ({ loggedIn, history }) => {
+    
     const redirectToParks = () => {
         history.push('/parks')
     }
     
     return (
         <div className="welcome">
-            { currentUser ? redirectToParks() : <p><LoginButton/> or <SignupButton/></p> }
+            { loggedIn ? redirectToParks() : <p><LoginButton/> or <SignupButton/></p> }
         </div>
     )
 }
 
 const mapStateToProps = ({ currentUser }) => {
     return {
-        currentUser
+        loggedIn: !!currentUser
     }
 }
 
