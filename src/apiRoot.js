14a03ae5243a51a88d463f2e5f8fd7df5e987aa7
github username <schacter.jon@gmail.com>
2021-04-16 15:44:17 -0700

    change to export default

--- src/apiRoot.js
+++ src/apiRoot.js
@@ -1,3 +1,3 @@
 const DEV_URL = 'http://localhost:3001/';
 const PROD_URL = 'https://nationalparktracker-api.herokuapp.com/';
-export const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
+export default process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
